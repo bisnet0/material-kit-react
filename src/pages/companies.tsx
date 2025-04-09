c33fa0bae98a549be0baff2699931c3197a74c01
bisneto <bis_henrique@yahoo.com.br>
2025-04-09 18:44:20 -0300

    fix: update page title from 'Users' to 'Empresas' for consistency

--- src/pages/companies.tsx
+++ src/pages/companies.tsx
@@ -7,7 +7,7 @@ import { UserView } from 'src/sections/companies/view';
 export default function Page() {
   return (
     <>
-      <title>{`Users - ${CONFIG.appName}`}</title>
+      <title>{`Empresas - ${CONFIG.appName}`}</title>
 
       <UserView />
     </>
