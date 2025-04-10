c4c93cd2e768215f9f5eea4d8e670bce5e24c838
bisneto <bis_henrique@yahoo.com.br>
2025-04-10 01:37:56 -0300

    feat: update OverviewAnalyticsView to fetch and display invoice download data

--- src/sections/overview/view/overview-analytics-view.tsx
+++ src/sections/overview/view/overview-analytics-view.tsx
@@ -1,85 +1,94 @@
-import Grid from '@mui/material/Grid';
+'use client';
+
+import { useEffect, useState } from 'react';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 
 import { DashboardContent } from 'src/layouts/dashboard';
-import { _posts, _tasks, _traffic, _timeline } from 'src/_mock';
-
-import { AnalyticsNews } from '../analytics-news';
-import { AnalyticsTasks } from '../analytics-tasks';
-import { AnalyticsCurrentVisits } from '../analytics-current-visits';
-import { AnalyticsOrderTimeline } from '../analytics-order-timeline';
-import { AnalyticsWebsiteVisits } from '../analytics-website-visits';
+import { getInvoicesDownloads } from 'src/middleware/apiMiddleware';
+import { fHour } from 'src/utils/format-time';
 import { AnalyticsWidgetSummary } from '../analytics-widget-summary';
-import { AnalyticsTrafficBySite } from '../analytics-traffic-by-site';
-import { AnalyticsCurrentSubject } from '../analytics-current-subject';
-import { AnalyticsConversionRates } from '../analytics-conversion-rates';
+import { AnalyticsCurrentVisits } from '../analytics-current-visits';
+import { typography } from 'src/theme';
 
 // ----------------------------------------------------------------------
 
 export function OverviewAnalyticsView() {
+  const [downloadsData, setDownloadsData] = useState<any | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getInvoicesDownloads();
+        setDownloadsData(data[0]); // assumindo que sempre tem ao menos 1
+      } catch (error) {
+        console.error('Erro ao carregar dados de downloads:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!downloadsData) {
+    return <Typography variant="h6">Carregando dados...</Typography>;
+  }
+
+  const lastDownloadDate = downloadsData.lastDownloads?.[0]?.downloadDate
+    ? fHour(downloadsData.lastDownloads[0].downloadDate)
+    : 'Sem dados';
+
   return (
     <DashboardContent maxWidth="xl">
       <Typography variant="h4" sx={{ mb: { xs: 3, md: 5 } }}>
-        Hi, Welcome back 👋
+        Dashboard
       </Typography>
 
       <Grid container spacing={3}>
         <Grid size={{ xs: 12, sm: 6, md: 3 }}>
           <AnalyticsWidgetSummary
-            title="Weekly sales"
-            percent={2.6}
-            total={714000}
-            icon={<img alt="Weekly sales" src="/assets/icons/glass/ic-glass-bag.svg" />}
+            title="Notas Fiscais Baixadas"
+            total={downloadsData.totalNfe}
+            icon={<img alt="Notas Fiscais Baixadas" src="/assets/icons/glass/ic-glass-invoices.svg" style={{ width: 40, height: 40 }} />}
             chart={{
-              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
-              series: [22, 8, 35, 50, 82, 84, 77, 12],
+              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+              series: [10, 20, 15, 30, 25, 10], // fictício
             }}
           />
         </Grid>
 
         <Grid size={{ xs: 12, sm: 6, md: 3 }}>
           <AnalyticsWidgetSummary
-            title="New users"
-            percent={-0.1}
-            total={1352831}
+            title="Média de Downloads por Dia"
+            total={downloadsData.averageDownloadsPerDay + "/dia"}
             color="secondary"
-            icon={<img alt="New users" src="/assets/icons/glass/ic-glass-users.svg" />}
+            icon={
+              <img
+          alt="Média de Downloads"
+          src="/assets/icons/glass/ic-glass-down.svg"
+          style={{ width: 40, height: 40 }} // Alterando o tamanho do ícone
+              />
+            }
             chart={{
-              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
-              series: [56, 47, 40, 62, 73, 30, 23, 54],
+              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+              series: [5, 10, 7, 12, 8, 10], // fictício
             }}
           />
         </Grid>
 
         <Grid size={{ xs: 12, sm: 6, md: 3 }}>
           <AnalyticsWidgetSummary
-            title="Purchase orders"
-            percent={2.8}
-            total={1723315}
+            title="Último Download Automático"
+            total={ "Hoje às "+ lastDownloadDate}
             color="warning"
-            icon={<img alt="Purchase orders" src="/assets/icons/glass/ic-glass-buy.svg" />}
+            icon={<img alt="Último download Automático" src="/assets/icons/glass/ic-glass-watch.svg" style={{ width: 40, height: 40 }} />}
             chart={{
-              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
-              series: [40, 70, 50, 28, 70, 75, 7, 64],
+              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+              series: [1, 2, 1, 3, 2, 1], // fictício
             }}
           />
         </Grid>
 
-        <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-          <AnalyticsWidgetSummary
-            title="Messages"
-            percent={3.6}
-            total={234}
-            color="error"
-            icon={<img alt="Messages" src="/assets/icons/glass/ic-glass-message.svg" />}
-            chart={{
-              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
-              series: [56, 30, 23, 54, 47, 40, 62, 73],
-            }}
-          />
-        </Grid>
-
-        <Grid size={{ xs: 12, md: 6, lg: 4 }}>
+        <Grid size={{ xs: 12, md: 6, lg: 6 }}>
           <AnalyticsCurrentVisits
             title="Current visits"
             chart={{
@@ -93,62 +102,18 @@ export function OverviewAnalyticsView() {
           />
         </Grid>
 
-        <Grid size={{ xs: 12, md: 6, lg: 8 }}>
-          <AnalyticsWebsiteVisits
-            title="Website visits"
-            subheader="(+43%) than last year"
+        <Grid size={{ xs: 12, md: 6, lg: 6 }}>
+          <AnalyticsCurrentVisits
+            title="Current visits"
             chart={{
-              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
               series: [
-                { name: 'Team A', data: [43, 33, 22, 37, 67, 68, 37, 24, 55] },
-                { name: 'Team B', data: [51, 70, 47, 67, 40, 37, 24, 70, 24] },
+                { label: 'America', value: 3500 },
+                { label: 'Asia', value: 2500 },
+                { label: 'Europe', value: 1500 },
+                { label: 'Africa', value: 500 },
               ],
             }}
           />
-        </Grid>
-
-        <Grid size={{ xs: 12, md: 6, lg: 8 }}>
-          <AnalyticsConversionRates
-            title="Conversion rates"
-            subheader="(+43%) than last year"
-            chart={{
-              categories: ['Italy', 'Japan', 'China', 'Canada', 'France'],
-              series: [
-                { name: '2022', data: [44, 55, 41, 64, 22] },
-                { name: '2023', data: [53, 32, 33, 52, 13] },
-              ],
-            }}
-          />
-        </Grid>
-
-        <Grid size={{ xs: 12, md: 6, lg: 4 }}>
-          <AnalyticsCurrentSubject
-            title="Current subject"
-            chart={{
-              categories: ['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math'],
-              series: [
-                { name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
-                { name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
-                { name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
-              ],
-            }}
-          />
-        </Grid>
-
-        <Grid size={{ xs: 12, md: 6, lg: 8 }}>
-          <AnalyticsNews title="News" list={_posts.slice(0, 5)} />
-        </Grid>
-
-        <Grid size={{ xs: 12, md: 6, lg: 4 }}>
-          <AnalyticsOrderTimeline title="Order timeline" list={_timeline} />
-        </Grid>
-
-        <Grid size={{ xs: 12, md: 6, lg: 4 }}>
-          <AnalyticsTrafficBySite title="Traffic by site" list={_traffic} />
-        </Grid>
-
-        <Grid size={{ xs: 12, md: 6, lg: 8 }}>
-          <AnalyticsTasks title="Tasks" list={_tasks} />
         </Grid>
       </Grid>
     </DashboardContent>
